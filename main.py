3db8973bd76831d5e3e6530302a10dcb09fd7c7a
goranaviani <goranaviani@gmail.com>
2020-05-16 21:00:50 +0200

    Parsing different date formats to SE format

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-
+import dateutil.parser
 def main():
-    pass
+    result = []
+    dates = ["2020-05-01", "June 1 1980", "12/15/2017", "12-25-2000"]
+    for date in dates:
+        fixed_date = dateutil.parser.parse(date).strftime("%Y-%m-%d")
+        result.append(fixed_date)
+
+    print(result)
+
 
 
 if __name__ == "__main__":
