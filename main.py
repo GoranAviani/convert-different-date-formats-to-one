e4447864bb47b901019e3c3ca2e61fad8d4d094b
goranaviani <goranaviani@gmail.com>
2020-05-16 21:15:10 +0200

    Fixed: parsing 60' as 1960

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ def main():
     result = []
     dates = ["2020-05-01", "June 1 1980", "12/15/2017", "12-25-2000",
              "23/1/99", "24/7/1999", "2/13/78", "02/05/88",
-             "01/04/65", "1/4/66"]
+             "01/04/65", "1/4/66", "8/18/12"]
     for date in dates:
         dt = dateutil.parser.parse(date)
         if dt.year > THRESHOLD:
             dt = dt.replace(year=dt.year - 100)
-        result.append(dt.strftime("%Y-%m-%d"))
+        dt_string = dt.strftime("%Y-%m-%d")
+        result.append(dt_string)
 
     print(result)
 
